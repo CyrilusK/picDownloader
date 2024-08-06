c19622adc4985533114d42d7ff8f92c527a4223c
CyrilusK <kcyrg17@gmail.com>
2024-08-06 14:08:10 +0300

    fix a bug with redirect into closed app

--- pictureDownloader/SceneDelegate.swift
+++ pictureDownloader/SceneDelegate.swift
@@ -24,14 +24,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         self.window?.makeKeyAndVisible()
         
         deepLinkHandler = DeepLinkHandler(tabBarController: initalVC)
+        
+        if let urlContext = connectionOptions.urlContexts.first {
+            deepLinkHandler?.handleDeepLink(urlContext.url)
+        }
     }
     
     func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
         guard let url = URLContexts.first?.url else { return }
         deepLinkHandler?.handleDeepLink(url)
     }
-    
-   
 
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.

a17e207958ac9bcc8c2d104e37e66d54e431440c
CyrilusK <kcyrg17@gmail.com>
2024-08-09 19:08:44 +0300

    add filters for pics

--- pictureDownloader/Constants.swift
+++ pictureDownloader/Constants.swift
@@ -14,3 +14,31 @@ struct Constants {
     static let dailyReminder = "dailyReminder"
     static let downloadSuccess = "downloadSuccess"
 }
+
+enum FilterTypes: String, CaseIterable {
+    case original = "Original"
+    case discBlur = "CIDiscBlur"
+    case chrome = "CIPhotoEffectChrome"
+    case fade = "CIPhotoEffectFade"
+    case instant = "CIPhotoEffectInstant"
+    case noir = "CIPhotoEffectNoir"
+    case process = "CIPhotoEffectProcess"
+    case tonal = "CIPhotoEffectTonal"
+    case transfer = "CIPhotoEffectTransfer"
+    case sepia = "CISepiaTone"
+    
+    var description: String {
+        switch self {
+        case .original: return "Original"
+        case .discBlur: return "Blur"
+        case .chrome: return "Chrome"
+        case .fade: return "Fade"
+        case .instant: return "Instant"
+        case .noir: return "Noir"
+        case .process: return "Process"
+        case .tonal: return "Tonal"
+        case .transfer: return "Transfer"
+        case .sepia: return "Sepia"
+        }
+    }
+}
--- pictureDownloader/ImageDetailModule/View/ImageDetailViewController.swift
+++ pictureDownloader/ImageDetailModule/View/ImageDetailViewController.swift
@@ -6,13 +6,16 @@
 //
 
 import UIKit
+import CoreImage
 
 final class ImageDetailViewController: UIViewController, ImageDetailViewInputProtocol {
     var output: ImageDetailOutputProtocol?
     
-    private var scrollView = UIScrollView()
+    private var zoomScrollView = UIScrollView()
+    private let filterScrollView = UIScrollView()
     private var imageView = UIImageView()
     private var closeButton = UIButton(type: .close)
+    private var originalImage: UIImage?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,38 +23,99 @@ final class ImageDetailViewController: UIViewController, ImageDetailViewInputPro
     }
     
     func setupUI(withImage image: UIImage) {
-        setupScrollView()
+        setupZoomScrollView()
+        setupFilterScrollView()
         setupImageView()
         setImage(image)
         setupCloseButton()
     }
     
-    private func setupScrollView() {
-        scrollView.backgroundColor = UIColor.gray.withAlphaComponent(0.5)
-        scrollView.translatesAutoresizingMaskIntoConstraints = false
-        scrollView.delegate = self
-        scrollView.minimumZoomScale = 1.0
-        scrollView.maximumZoomScale = 10.0
-        view.addSubview(scrollView)
+    private func setupFilterScrollView() {
+        filterScrollView.showsHorizontalScrollIndicator = false
+        filterScrollView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(filterScrollView)
         
         NSLayoutConstraint.activate([
-            scrollView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            scrollView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            filterScrollView.leadingAnchor.constraint(equalTo: zoomScrollView.leadingAnchor),
+            filterScrollView.trailingAnchor.constraint(equalTo: zoomScrollView.trailingAnchor),
+            filterScrollView.topAnchor.constraint(equalTo: zoomScrollView.bottomAnchor),
+            filterScrollView.heightAnchor.constraint(equalToConstant: 90)
+        ])
+        setupFilterButtons()
+    }
+    
+    private func setupFilterButtons() {
+        var xOffset: CGFloat = 10.0
+        
+        for filter in FilterTypes.allCases {
+            let button = UIButton(type: .system)
+            button.setTitle(filter.description, for: .normal)
+            button.setTitleColor(.white, for: .normal)
+            button.addTarget(self, action: #selector(filterButtonTapped), for: .touchUpInside)
+            button.frame = CGRect(x: xOffset, y: 10, width: 70, height: 70)
+            button.imageView?.contentMode = .scaleAspectFill
+            button.layer.cornerRadius = 25
+            button.clipsToBounds = true
+            filterScrollView.addSubview(button)
+            
+            xOffset += button.frame.width + 10
+            
+            DispatchQueue.main.async {
+                guard let image = self.imageView.image else { return }
+                let filteredImage = filter == .original ? image.cgImage : self.applyFilter(named: filter.rawValue, with: image)
+                if let cgImage = filteredImage {
+                    button.setBackgroundImage(UIImage(cgImage: cgImage), for: .normal)
+                }
+            }
+        }
+        filterScrollView.contentSize = CGSize(width: xOffset, height: 70)
+    }
+    
+    @objc private func filterButtonTapped(_ sender: UIButton) {
+        guard let filterName = sender.titleLabel?.text,
+              let filterType = FilterTypes.allCases.first(where: { $0.description == filterName }) else { return }
+        guard let image = imageView.image else { return }
+        
+        guard let filteredImage = filterType == .original ? originalImage?.cgImage : applyFilter(named: filterType.rawValue, with: image) else { return }
+        imageView.image = UIImage(cgImage: filteredImage)
+    }
+    
+    private func applyFilter(named filterName: String, with image: UIImage) -> CGImage? {
+        let context = CIContext(options: nil)
+        let ciImage = CIImage(image: image)
+        guard let filter = CIFilter(name: filterName) else { return nil }
+        filter.setValue(ciImage, forKey: kCIInputImageKey)
+        guard let outputImage = filter.outputImage else { return nil }
+        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return nil }
+        return cgImage
+    }
+    
+    private func setupZoomScrollView() {
+        zoomScrollView.backgroundColor = UIColor.gray.withAlphaComponent(0.5)
+        zoomScrollView.translatesAutoresizingMaskIntoConstraints = false
+        zoomScrollView.delegate = self
+        zoomScrollView.minimumZoomScale = 1.0
+        zoomScrollView.maximumZoomScale = 10.0
+        view.addSubview(zoomScrollView)
+        
+        NSLayoutConstraint.activate([
+            zoomScrollView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            zoomScrollView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
         ])
     }
     
     private func setupImageView() {
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.contentMode = .scaleAspectFit
-        scrollView.addSubview(imageView)
+        zoomScrollView.addSubview(imageView)
         
         NSLayoutConstraint.activate([
-            imageView.topAnchor.constraint(equalTo: scrollView.topAnchor),
-            imageView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
-            imageView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
-            imageView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
-            imageView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
-            imageView.heightAnchor.constraint(equalTo: scrollView.heightAnchor)
+            imageView.topAnchor.constraint(equalTo: zoomScrollView.topAnchor),
+            imageView.leadingAnchor.constraint(equalTo: zoomScrollView.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: zoomScrollView.trailingAnchor),
+            imageView.bottomAnchor.constraint(equalTo: zoomScrollView.bottomAnchor),
+            imageView.widthAnchor.constraint(equalTo: zoomScrollView.widthAnchor),
+            imageView.heightAnchor.constraint(equalTo: zoomScrollView.heightAnchor)
         ])
     }
     
@@ -62,15 +126,16 @@ final class ImageDetailViewController: UIViewController, ImageDetailViewInputPro
         view.addSubview(closeButton)
         
         NSLayoutConstraint.activate([
-            closeButton.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 5),
-            closeButton.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -5)
+            closeButton.topAnchor.constraint(equalTo: zoomScrollView.topAnchor, constant: 5),
+            closeButton.trailingAnchor.constraint(equalTo: zoomScrollView.trailingAnchor, constant: -5)
         ])
     }
     
     private func setImage(_ image: UIImage) {
+        originalImage = image
         imageView.image = image
         imageView.sizeToFit()
-        scrollView.contentSize = imageView.bounds.size
+        zoomScrollView.contentSize = imageView.bounds.size
 
         let imageWidth = image.size.width
         let imageHeight = image.size.height
@@ -84,8 +149,8 @@ final class ImageDetailViewController: UIViewController, ImageDetailViewInputPro
         let newHeight = imageHeight * scale
 
         NSLayoutConstraint.activate([
-            scrollView.widthAnchor.constraint(equalToConstant: newWidth),
-            scrollView.heightAnchor.constraint(equalToConstant: newHeight),
+            zoomScrollView.widthAnchor.constraint(equalToConstant: newWidth),
+            zoomScrollView.heightAnchor.constraint(equalToConstant: newHeight),
         ])
     }
     
